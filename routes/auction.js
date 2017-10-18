66bd2c0c6f384b71fcd40bef866fad5d84b12c7a
FengHongying <55503903@qq.com>
2017-10-18 10:05:33 +0800

    測試中間的修改

---
+++ routes/auction.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var io = require('socket.io');
+var fs = require("fs");
+/* GET home page. */
+
+
+
+router.get('/', function(req, res, next) {
+
+    /*async function run() {
+        var a = await sqlasnyc("select * from `mvm_config` limit 1");
+        console.log(a);
+    }
+    run();*/
+
+});
+
+
+module.exports = router;
